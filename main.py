17ca878cbec03e96ef1888027d81af39b574e2da
Ritik Bompilwar <ritik19101@iiitnr.edu.in>
2022-12-23 21:10:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ from ontology_dc8f06af066e4a7880a5938933236037.simple_text import SimpleText
 from openfabric_pysdk.context import OpenfabricExecutionRay
 from openfabric_pysdk.loader import ConfigClass
 from time import time
-
+from transformers import pipeline
+sci_model = pipeline(model="model-attribution-challenge/xlnet-base-cased")
 
 ############################################################
 # Callback function called on update config
 ############################################################
 def config(configuration: ConfigClass):
-    # TODO Add code here
+    #Add code here
     pass
 
 
@@ -21,8 +22,12 @@ def config(configuration: ConfigClass):
 def execute(request: SimpleText, ray: OpenfabricExecutionRay) -> SimpleText:
     output = []
     for text in request.text:
-        # TODO Add code here
-        response = ''
+        #Add code here
+        response = sci_model(text_inputs=request.text)
+        #print(response)
+        response = response[0][0]['generated_text'][0:200]
         output.append(response)
+        #print(response)
+    print(output)
 
     return SimpleText(dict(text=output))
